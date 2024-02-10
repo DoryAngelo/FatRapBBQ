847a26d410bf8f9770958e806cb508402522f22b
Darrie Angelo <darriedemesa@gmail.com>
2024-02-10 22:13:55 +0800

    Added backend for cart

--- Website/cart.php
+++ Website/cart.php
@@ -1,6 +1,7 @@
 <?php
 
 @include 'constants.php';
+
 $PRSN_ID = $_SESSION['prsn_id'];
 
 ?>
@@ -12,9 +13,10 @@ $PRSN_ID = $_SESSION['prsn_id'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <!--change title-->
+    <title>Cart</title>
     <link rel="stylesheet" href="header-styles.css">
-    <link rel="stylesheet" href="login-styles.css">
+    <link rel="stylesheet" href="customer-styles.css"><!--change css file-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
@@ -54,20 +56,76 @@ $PRSN_ID = $_SESSION['prsn_id'];
         </div>
     </header>
     <main>
-        <div>
-        <h1>Cart</h1>
+        <section class="section cart">
+            <div class="section-heading">
+                <h2>Cart</h2>
+            </div>
+            <section class="section-body">
+                <section class="block">
+                    <div class="block-body">
+                        <div class="table-wrap">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <td></td>
+                                        <th><span class="sr-only year">Quantity</span></th>
+                                        <th><span class="sr-only year">Price</span></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
 
-        </div>
-        <div>
-            
+                                    $CUS_ID = $_SESSION['prsn_id'];
 
-        </div>
-        
-        <div>
-            <a href="checkout.php" class="secondary-btn">Checkout</a>
-        </div>
+                                    $sql = "SELECT IN_ORDER_ID, FOOD_NAME, FOOD_IMG, FOOD_PRICE, CUS_ID, IN_ORDER_QUANTITY, IN_ORDER_TOTAL 
+                                FROM food, in_order WHERE food.FOOD_ID = in_order.FOOD_ID AND IN_ORDER_STATUS != 'Delivered'";
+
+                                    $res = mysqli_query($conn, $sql);
+
+                                    $count = mysqli_num_rows($res);
+
+                                    if ($count > 0) {
+                                        while ($row = mysqli_fetch_assoc($res)) {
+                                            if ($row['CUS_ID'] == $CUS_ID) {
+                                                $IN_ORDER_ID = $row['IN_ORDER_ID'];
+                                                $FOOD_NAME = $row['FOOD_NAME'];
+                                                $FOOD_PRICE = $row['FOOD_PRICE'];
+                                                $FOOD_IMG = $row['FOOD_IMG'];
+                                                $IN_ORDER_QUANTITY = $row['IN_ORDER_QUANTITY'];
+                                                $IN_ORDER_TOTAL = $row['IN_ORDER_TOTAL'];
+                                    ?>
+                                                <div class="green">
+                                                    <tr class="data">
+                                                        <th><?php echo $FOOD_NAME ?><span class="description"></span></th>
+                                                        <td><?php echo $IN_ORDER_QUANTITY ?></td>
+                                                        <td>₱<?php echo $FOOD_PRICE ?></td>
+                                                    </tr>
+                                                </div>
+                                    <?php
+                                            }
+                                        }
+                                    }
+                                    $sql2 = "SELECT SUM(IN_ORDER_TOTAL) AS Total FROM  IN_ORDER WHERE CUS_ID = $CUS_ID";
+                                    $res2 = mysqli_query($conn, $sql2);
+                                    $row2 = mysqli_fetch_assoc($res2);
+                                    $total = $row2['Total'];
+                                    ?>
+                                    <tr class="total">
+                                        <th></th>
+                                        <td>Total:</td>
+                                        <td class="current">₱<?php echo $total?></td>
+                                    </tr>
+
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </section>
+                <a href="checkout.php" class="page-button center">Checkout</a>
+            </section>
+        </section>
     </main>
-
     <footer>
         <div class="footer-container">
             <div class="left-container">
