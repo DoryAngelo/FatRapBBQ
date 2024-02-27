89c7e5c81677ff47ab1faff5e8260c2aaab43d7e
htrcrd <117492863+htrcrd@users.noreply.github.com>
2024-02-27 14:52:08 +0800

    improved wholesale cus register page

--- Website/wc-register.php
+++ Website/wc-register.php
@@ -45,12 +45,15 @@ if(isset($_POST['submit'])){
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet"> 
         <script src="app.js" defer></script>
+        <!-- add the code below to load the icons -->
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    
     </head>
     <body>
         <header>
             <div class="header-container">
                 <div class="website-title">
-                    <img id="logo" src="images/client-logo.jpg">
+                    <img id="logo" src="images/client-logo.png">
                     <div class="text">
                         <h1>Fat Rap's Barbeque's Online Store</h1>
                     </div>
@@ -99,6 +102,11 @@ if(isset($_POST['submit'])){
                             <svg class="showcpass" xmlns="http://www.w3.org/2000/svg" style="vertical-align: -0.125em;" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5s5 2.24 5 5s-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3s-1.34-3-3-3z"/></svg>
                             <svg class="hidecpass" xmlns="http://www.w3.org/2000/svg" style="vertical-align: -0.125em;" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M11.83 9L15 12.16V12a3 3 0 0 0-3-3h-.17m-4.3.8l1.55 1.55c-.05.21-.08.42-.08.65a3 3 0 0 0 3 3c.22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53a5 5 0 0 1-5-5c0-.79.2-1.53.53-2.2M2 4.27l2.28 2.28l.45.45C3.08 8.3 1.78 10 1 12c1.73 4.39 6 7.5 11 7.5c1.55 0 3.03-.3 4.38-.84l.43.42L19.73 22L21 20.73L3.27 3M12 7a5 5 0 0 1 5 5c0 .64-.13 1.26-.36 1.82l2.93 2.93c1.5-1.25 2.7-2.89 3.43-4.75c-1.73-4.39-6-7.5-11-7.5c-1.4 0-2.74.25-4 .7l2.17 2.15C10.74 7.13 11.35 7 12 7Z"/></svg>
                         </div>
+                        <div class="form-field-input">
+                            <label for="valid-id">Valid ID</label>
+                            <p class="label-desc">(accepted files: .jpg, .png)</p>
+                            <input class="" type="file" id="valid-id" name="valid-id" required ><!-- numbers only, starts with 09, must have 11-digits -->
+                        </div>
                         <button class="primary-btn" name="submit">Register</button>
                         <p class="small-text">Already have an account? <a class="login-link" href="login-page.php">Login</a></p>	
                     </div>
@@ -112,33 +120,39 @@ if(isset($_POST['submit'])){
                     <div class="list">
                         <ul>
                             <li><a href="#">Home</a></li>
-                            <li><a href="#">Menu</a></li>
-                            <li><a href="#">Cart</a></li>
-                            <li><a href="#">Track order</a></li>
+                            <li><a href="<?php echo SITEURL; ?>menu.php">Menu</a></li>
+                            <li><a href="<?php echo SITEURL; ?>cart.php">Cart</a></li>
+                            <li><a href="<?php echo SITEURL; ?>track-order.php">Track order</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="right-container">
                     <div class="icons-block">
-                        <img id="logo" src="images/circle logo.png">
-                        <img id="logo" src="images/circle logo.png">
-                        <img id="logo" src="images/circle logo.png">
+                        <a href="https://www.youtube.com/">
+                            <i class='bx bxl-facebook-circle'></i>
+                        </a>
+                        <a href="https://www.youtube.com/">
+                            <i class='bx bxl-tiktok'></i>
+                        </a>
+                        <a href="https://www.youtube.com/">
+                            <i class='bx bxl-instagram' ></i>
+                        </a>
                     </div>
                     <div class="list">
                         <div class="list-items">
-                            <!--insert icon-->
+                            <i class='bx bxs-envelope' ></i>
                             <p>email@gmail.com</p>
                         </div>
                         <div class="list-items">
-                            <!--insert icon-->
+                            <i class='bx bxs-phone'></i>
                             <p>0912 345 6789 | 912 1199</p>
                         </div>
                         <div class="list-items">
-                            <!--insert icon-->
+                            <i class='bx bxs-map' ></i>
                             <p>123 Magaling St., Brgy. Something, Somewhere City</p>
                         </div>
                     </div>
-                </div>    
+                </div>
             </div>
         </footer>
     </body>
