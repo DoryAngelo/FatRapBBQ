0a3ad802980a43b2c59d9f51373b55697f61e16c
htrcrd <117492863+htrcrd@users.noreply.github.com>
2024-02-27 10:18:56 +0800

    improved cart page (needs backend fixing)

--- Website/cart.php
+++ Website/cart.php
@@ -21,13 +21,16 @@ $PRSN_ID = $_SESSION['prsn_id'];
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <script src="app.js" defer></script>
+    <!-- add the code below to load the icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    
 </head>
 
 <body>
     <header>
         <div class="header-container">
             <div class="website-title">
-                <img id="logo" src="images/client-logo.jpg">
+                <img id="logo" src="images/client-logo.png">
                 <div class="text">
                     <h1>Fat Rap's Barbeque's Online Store</h1>
                 </div>
@@ -64,61 +67,70 @@ $PRSN_ID = $_SESSION['prsn_id'];
                 <section class="block">
                     <div class="block-body">
                         <div class="table-wrap">
-                            <table>
+                            <table  class="order">
                                 <thead>
                                     <tr>
-                                        <td></td>
-                                        <th><span class="sr-only year">Quantity</span></th>
-                                        <th><span class="sr-only year">Price</span></th>
+                                        <th class="header first-col"></th>
+                                        <th class="header narrow-col">Quantity</th>
+                                        <th class="header narrow-col price-col">Price</th>
+                                        <th class="header narrow-col"></th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php
-
-                                    $CUS_ID = $_SESSION['prsn_id'];
-
-                                    $sql = "SELECT IN_ORDER_ID, FOOD_NAME, FOOD_IMG, FOOD_PRICE, CUS_ID, IN_ORDER_QUANTITY, IN_ORDER_TOTAL 
-                                FROM food, in_order WHERE food.FOOD_ID = in_order.FOOD_ID AND IN_ORDER_STATUS != 'Delivered' AND CUS_ID = $CUS_ID";
-
-                                    $res = mysqli_query($conn, $sql);
-
-                                    $count = mysqli_num_rows($res);
-
-                                    if ($count > 0) {
-                                        while ($row = mysqli_fetch_assoc($res)) {
-
-                                            $IN_ORDER_ID = $row['IN_ORDER_ID'];
-                                            $FOOD_NAME = $row['FOOD_NAME'];
-                                            $FOOD_PRICE = $row['FOOD_PRICE'];
-                                            $FOOD_IMG = $row['FOOD_IMG'];
-                                            $IN_ORDER_QUANTITY = $row['IN_ORDER_QUANTITY'];
-                                            $IN_ORDER_TOTAL = $row['IN_ORDER_TOTAL'];
+                                    // $CUS_ID = $_SESSION['prsn_id'];
+                                    // $sql = "SELECT IN_ORDER_ID, FOOD_NAME, FOOD_IMG, FOOD_PRICE, CUS_ID, IN_ORDER_QUANTITY, IN_ORDER_TOTAL 
+                                    // FROM food, in_order WHERE food.FOOD_ID = in_order.FOOD_ID AND IN_ORDER_STATUS != 'Delivered' AND CUS_ID = $CUS_ID";
+                                    // $res = mysqli_query($conn, $sql);
+                                    // $count = mysqli_num_rows($res);
+                                    // if ($count > 0) {
+                                    //     while ($row = mysqli_fetch_assoc($res)) {
+                                    //         $IN_ORDER_ID = $row['IN_ORDER_ID'];
+                                    //         $FOOD_NAME = $row['FOOD_NAME'];
+                                    //         $FOOD_PRICE = $row['FOOD_PRICE'];
+                                    //         $FOOD_IMG = $row['FOOD_IMG'];
+                                    //         $IN_ORDER_QUANTITY = $row['IN_ORDER_QUANTITY'];
+                                    //         $IN_ORDER_TOTAL = $row['IN_ORDER_TOTAL'];
                                     ?>
-                                            <div class="green">
+                                            <!-- <div class="green">
                                                 <tr class="data">
                                                     <th><?php echo $FOOD_NAME ?><span class="description"></span></th>
                                                     <td><?php echo $IN_ORDER_QUANTITY ?></td>
                                                     <td>₱<?php echo $FOOD_PRICE ?></td>
                                                 </tr>
-                                            </div>
+                                            </div> -->
                                     <?php
-
-                                        }
-                                    }
-                                    $sql2 = "SELECT SUM(IN_ORDER_TOTAL) AS Total FROM  IN_ORDER WHERE CUS_ID = $CUS_ID";
-                                    $res2 = mysqli_query($conn, $sql2);
-                                    $row2 = mysqli_fetch_assoc($res2);
-                                    $total = $row2['Total'];
+                                    //     }
+                                    // }
+                                    // $sql2 = "SELECT SUM(IN_ORDER_TOTAL) AS Total FROM  IN_ORDER WHERE CUS_ID = $CUS_ID";
+                                    // $res2 = mysqli_query($conn, $sql2);
+                                    // $row2 = mysqli_fetch_assoc($res2);
+                                    // $total = $row2['Total'];
                                     ?>
-                                    <tr class="total">
-                                        <th></th>
-                                        <td>Total:</td>
-                                        <td class="current">₱<?php echo $total ?></td>
+                                    <tr> <!-- one row for a product-->
+                                        <td data-cell="customer" class="first-col">
+                                            <div class="pic-grp">
+                                                <img src="https://urbanblisslife.com/wp-content/uploads/2021/06/Filipino-Pork-BBQ-FEATURE.jpg" alt="">
+                                                <p>Pork BBQ</p>
+                                            </div>
+                                        </td> <!--Pic and Name-->
+                                        <td class="narrow-col quantity-col">
+                                            <div class="quantity-grp">
+                                                <i class='bx bxs-minus-circle js-minus'></i>
+                                                <p class="amount js-num">1</p>
+                                                <i class='bx bxs-plus-circle js-plus'></i>
+                                            </div>
+                                            <p class="remaining">100 sticks remaining</p>
+                                        </td> <!--Quantity-->
+                                        <td class="narrow-col price-col">₱25.00</td><!--Price-->
+                                        <td class="narrow-col"><i class='bx bxs-trash-alt trash'></i></td><!--Action-->
                                     </tr>
-
-
                                 </tbody>
                             </table>
+                        </div>
+                        <div class="payment">
+                            <h3>Total Payment:</h3>
+                            <h3>₱250.00</h3>
                         </div>
                     </div>
                 </section>
@@ -133,35 +145,63 @@ $PRSN_ID = $_SESSION['prsn_id'];
                 <div class="list">
                     <ul>
                         <li><a href="#">Home</a></li>
-                        <li><a href="#">Menu</a></li>
-                        <li><a href="#">Cart</a></li>
-                        <li><a href="#">Track order</a></li>
+                        <li><a href="<?php echo SITEURL; ?>menu.php">Menu</a></li>
+                        <li><a href="<?php echo SITEURL; ?>cart.php">Cart</a></li>
+                        <li><a href="<?php echo SITEURL; ?>track-order.php">Track order</a></li>
                     </ul>
                 </div>
             </div>
             <div class="right-container">
                 <div class="icons-block">
-                    <img id="logo" src="images/circle logo.png">
-                    <img id="logo" src="images/circle logo.png">
-                    <img id="logo" src="images/circle logo.png">
+                    <a href="https://www.youtube.com/">
+                        <i class='bx bxl-facebook-circle'></i>
+                    </a>
+                    <a href="https://www.youtube.com/">
+                        <i class='bx bxl-tiktok'></i>
+                    </a>
+                    <a href="https://www.youtube.com/">
+                        <i class='bx bxl-instagram' ></i>
+                    </a>
                 </div>
                 <div class="list">
                     <div class="list-items">
-                        <!--insert icon-->
+                        <i class='bx bxs-envelope' ></i>
                         <p>email@gmail.com</p>
                     </div>
                     <div class="list-items">
-                        <!--insert icon-->
+                        <i class='bx bxs-phone'></i>
                         <p>0912 345 6789 | 912 1199</p>
                     </div>
                     <div class="list-items">
-                        <!--insert icon-->
+                        <i class='bx bxs-map' ></i>
                         <p>123 Magaling St., Brgy. Something, Somewhere City</p>
                     </div>
                 </div>
             </div>
         </div>
     </footer>
-</body>
+    <script> /*js code for the increment and decrement buttons for the quantity*/
+            const plus = document.querySelector(".js-plus"),
+                minus = document.querySelector(".js-minus"),
+                num = document.querySelector(".js-num");
 
+            let a = 1;
+
+            plus.addEventListener("click", ()=>{
+                a++;
+                console.log(a);
+                num.innerText = a;
+            } 
+            );
+
+            minus.addEventListener("click", ()=>{
+                if(a > 1) {
+                    a--;
+                    console.log(a);
+                    num.innerText = a;
+                }
+            } 
+            );
+        </script>
+</body>
 </html>
