5c2f0517095dcd00d3ed3d216c0a0f7efe6d4bc0
htrcrd <117492863+htrcrd@users.noreply.github.com>
2024-02-28 18:25:21 +0800

    improved cus register page: header and footer

--- Website/cus-register-page.php
+++ Website/cus-register-page.php
@@ -38,33 +38,25 @@ if(isset($_POST['submit'])){
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Register</title>
+        <title>Register | Fat Rap's Barbeque's Online Store</title>
         <link rel="stylesheet" href="header-styles.css">
         <link rel="stylesheet" href="cus-reg-styles.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet"> 
         <script src="app.js" defer></script>
+        <!-- add the code below to load the icons -->
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     </head>
     <body>
         <header>
             <div class="header-container">
                 <div class="website-title">
-                    <img id="logo" src="images/client-logo.jpg">
+                    <img id="logo" src="images/client-logo.png">
                     <div class="text">
                         <h1>Fat Rap's Barbeque's Online Store</h1>
                     </div>
                 </div>
-                <nav>
-                    <ul>
-                        <!--TODO: ADD LINKS-->
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Menu</a></li>
-                        <li><a href="#">Cart</a></li>
-                        <!-- Text below should change to 'Logout'once user logged in-->
-                        <li><a href="login-page.php">Login</a></li>
-                    </ul>
-                </nav>
             </div>
         </header>
         <main>
@@ -106,40 +98,46 @@ if(isset($_POST['submit'])){
             </div>
         </main>
         <footer>
-            <div class="footer-container">
-                <div class="left-container">
-                    <h1>Fat Rap's Barbeque's Online Store</h1>
-                    <div class="list">
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Menu</a></li>
-                            <li><a href="#">Cart</a></li>
-                            <li><a href="#">Track order</a></li>
-                        </ul>
+        <div class="footer-container">
+            <div class="left-container">
+                <h1>Fat Rap's Barbeque's Online Store</h1>
+                <div class="list">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="<?php echo SITEURL; ?>menu.php">Menu</a></li>
+                        <li><a href="<?php echo SITEURL; ?>cart.php">Cart</a></li>
+                        <li><a href="<?php echo SITEURL; ?>track-order.php">Track order</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="right-container">
+                <div class="icons-block">
+                    <a href="https://www.youtube.com/">
+                        <i class='bx bxl-facebook-circle'></i>
+                    </a>
+                    <a href="https://www.youtube.com/">
+                        <i class='bx bxl-tiktok'></i>
+                    </a>
+                    <a href="https://www.youtube.com/">
+                        <i class='bx bxl-instagram' ></i>
+                    </a>
+                </div>
+                <div class="list">
+                    <div class="list-items">
+                        <i class='bx bxs-envelope' ></i>
+                        <p>email@gmail.com</p>
+                    </div>
+                    <div class="list-items">
+                        <i class='bx bxs-phone'></i>
+                        <p>0912 345 6789 | 912 1199</p>
+                    </div>
+                    <div class="list-items">
+                        <i class='bx bxs-map' ></i>
+                        <p>123 Magaling St., Brgy. Something, Somewhere City</p>
                     </div>
                 </div>
-                <div class="right-container">
-                    <div class="icons-block">
-                        <img id="logo" src="images/circle logo.png">
-                        <img id="logo" src="images/circle logo.png">
-                        <img id="logo" src="images/circle logo.png">
-                    </div>
-                    <div class="list">
-                        <div class="list-items">
-                            <!--insert icon-->
-                            <p>email@gmail.com</p>
-                        </div>
-                        <div class="list-items">
-                            <!--insert icon-->
-                            <p>0912 345 6789 | 912 1199</p>
-                        </div>
-                        <div class="list-items">
-                            <!--insert icon-->
-                            <p>123 Magaling St., Brgy. Something, Somewhere City</p>
-                        </div>
-                    </div>
-                </div>    
             </div>
-        </footer>
+        </div>
+    </footer>
     </body>
 </html>
--- Website/login-page.php
+++ Website/login-page.php
@@ -48,7 +48,7 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login | Fat Rap's Barbeque's Online Store</title>
     <link rel="stylesheet" href="header-styles.css">
     <link rel="stylesheet" href="login-styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
