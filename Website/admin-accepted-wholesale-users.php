7435ad8dc252dc9729880f10869acd8460cc0742
Darrie Angelo <darriedemesa@gmail.com>
2024-03-01 21:50:24 +0800

    admin can now accept and reject wholesaler application

--- Website/admin-accepted-wholesale-users.php
+++ Website/admin-accepted-wholesale-users.php
@@ -4,163 +4,147 @@
 
 $PRSN_ID = $_SESSION['prsn_id'];
 
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!--change title-->
-        <title>Accepted Wholesale Users | Admin</title>
-        <link rel="stylesheet" href="header-styles.css">
-        <link rel="stylesheet" href="admin-styles.css"><!--change css file-->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet"> 
-        <script src="app.js" defer></script>
-        <!-- add the code below to load the icons -->
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    </head>
-    <body>
-        <header class="backend">
-            <div class="header-container">
-                <div class="website-title">
-                    <img id="logo" src="images/client-logo.png">
-                    <div class="text">
-                        <h1>Fat Rap's Barbeque's Online Store</h1>
-                        <p>ADMIN</p>
-                    </div>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--change title-->
+    <title>Accepted Wholesale Users | Admin</title>
+    <link rel="stylesheet" href="header-styles.css">
+    <link rel="stylesheet" href="admin-styles.css"><!--change css file-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <script src="app.js" defer></script>
+    <!-- add the code below to load the icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <header class="backend">
+        <div class="header-container">
+            <div class="website-title">
+                <img id="logo" src="images/client-logo.png">
+                <div class="text">
+                    <h1>Fat Rap's Barbeque's Online Store</h1>
+                    <p>ADMIN</p>
                 </div>
-                <nav>
-                    <ul>
-                        <li><a href="<?php echo SITEURL ;?>admin-home.php">Home</a></li>
-                        <li><a href="<?php echo SITEURL ;?>admin-edit-menu.php">Menu</a></li>
-                        <li><a href="<?php echo SITEURL ;?>admin-new-orders.php">Orders</a></li>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo SITEURL; ?>admin-home.php">Home</a></li>
+                    <li><a href="<?php echo SITEURL; ?>admin-edit-menu.php">Menu</a></li>
+                    <li><a href="<?php echo SITEURL; ?>admin-new-orders.php">Orders</a></li>
+                    <?php
+                    if (isset($_SESSION['prsn_id'])) {
+                    ?>
+                        <li><a href="<?php echo SITEURL; ?>logout.php">Logout</a>
+                        <li>
                         <?php
-                            if(isset($_SESSION['prsn_id'])){
-                        ?>  
-                            <li><a href="<?php echo SITEURL ;?>logout.php">Logout</a><li>
-                        <?php
-                            } 
-                            else 
-                            {
+                    } else {
                         ?>
-                            <li><a href="<?php echo SITEURL ;?>login-page.php">Login</a></li>
+                        <li><a href="<?php echo SITEURL; ?>login-page.php">Login</a></li>
+                    <?php
+                    }
+                    ?>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section class="section">
+            <div class="section-heading">
+                <h2>Accepted Wholesale Users</h2>
+                <div class="inline">
+                    <p>Filter by date:</p>
+                    <input type="date">
+                </div>
+            </div>
+            <section class="section-body">
+                <section class="main-section table-wrapper">
+                    <table class="alternating">
+                        <tr>
+                            <th class="header">Date and Time</th>
+                            <th class="header">Application #</th>
+                            <th class="header">Name</th>
+                            <th class="header">Email</th>
+                            <th class="header">Confirmed</th>
+                        </tr>
+                        <?php
+                        $sql = "SELECT * FROM person, wholesaler WHERE wholesaler.PRSN_ID = person.PRSN_ID AND PRSN_ROLE = 'Wholesaler' ";
+                        $res = mysqli_query($conn, $sql);
+                        $count = mysqli_num_rows($res);
+
+                        if ($count > 0) {
+                            while ($row = mysqli_fetch_assoc($res)) {
+                                $DATE_OF_REGISTRATION = $row['DATE_OF_REGISTRATION'];
+                                $WHL_ID = $row['WHL_ID'];
+                                $PRSN_NAME = $row['PRSN_NAME'];
+                                $PRSN_EMAIL = $row['PRSN_EMAIL'];
+                                $WHL_IMAGE = $row['WHL_IMAGE'];
+                        ?>
+                                <tr>
+                                    <td data-cell="Date and Time"><?php echo $DATE_OF_REGISTRATION ?></td>
+                                    <td data-cell="Application #"><a href="<?php echo SITEURL ?>admin-application-details.php?WHL_ID=<?php echo $WHL_ID; ?>&PRSN_NAME=<?php echo $PRSN_NAME ?>&PRSN_EMAIL=<?php echo $PRSN_EMAIL ?>&WHL_IMAGE=<?php echo $WHL_IMAGE ?>"><?php echo $WHL_ID ?></a></td>
+                                    <td data-cell="Name"><?php echo $PRSN_NAME ?></td>
+                                    <td data-cell="Email"><?php echo $PRSN_EMAIL ?></td>
+                                    <td data-cell="Confimed">
+                                        <div class="btn-wrapper">
+                                            <form method="post">
+                                                <button name="accept" class="btn-check"><i class='bx bxs-check-circle'></i></button>
+                                                <button name="reject" class="btn-cross"><i class='bx bxs-x-circle'></i></button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
                         <?php
                             }
+                        } else {
+                            echo "<div class='error'>No Order Found</div>";
+                        }
                         ?>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <main>
-            <section class="section">
-                <div class="section-heading">
-                    <h2>Accepted Wholesale Users</h2>
-                    <div class="inline">
-                        <p>Filter by date:</p>
-                        <input type="date">
-                    </div>
-                </div> 
-                <section class="section-body">
-                    <section class="main-section table-wrapper">
-                        <table class="alternating">
-                            <tr>
-                                <th class="header">Date and Time</th>
-                                <th class="header">Application #</th>
-                                <th class="header">Name</th>
-                                <th class="header">Email</th>
-                                <th class="header">Confirmed</th>
-                            </tr>
-                            <!-- PLACEHOLDER TABLE ROWS FOR FRONTEND TESTING PURPOSES -->
-                            <tr>
-                                <td data-cell="Date and Time">8/5/23 3:5:15 PM</td>
-                                <td data-cell="Application #">0325</td>
-                                <td data-cell="Name">John Doe</td>
-                                <td data-cell="Email">johndoe@email.com</td>
-                                <td data-cell="Confimed">
-                                    <div class="btn-wrapper">
-                                        <button class="btn-check"><i class='bx bxs-check-circle'></i></button>
-                                        <button class="btn-cross"><i class='bx bxs-x-circle'></i></button>
-                                    </div>
-                                </td>
-                            </tr>
-                                <td data-cell="Date and Time">8/5/23 3:5:15 PM</td>
-                                <td data-cell="Application #">0325</td>
-                                <td data-cell="Name">John Doe</td>
-                                <td data-cell="Email">johndoe@email.com</td>
-                                <td data-cell="Confimed"></td>
-                            </tr>
-                                <td data-cell="Date and Time">8/5/23 3:5:15 PM</td>
-                                <td data-cell="Application #">0325</td>
-                                <td data-cell="Name">John Doe</td>
-                                <td data-cell="Email">johndoe@email.com</td>
-                                <td data-cell="Confimed">
-                                </td>
-                            </tr>
-                                <td data-cell="Date and Time">8/5/23 3:5:15 PM</td>
-                                <td data-cell="Application #">0325</td>
-                                <td data-cell="Name">John Doe</td>
-                                <td data-cell="Email">johndoe@email.com</td>
-                                <td data-cell="Confimed"></td>
-                            </tr>
-                            <!-- <?php
-                                $sql = "SELECT * FROM placed_order";
-                                $res = mysqli_query($conn, $sql);
-                                $count = mysqli_num_rows($res);
-
-                                if($count>0)
-                                {
-                                    while($row=mysqli_fetch_assoc($res))
-                                    {
-                                        $PLACED_ORDER_ID = $row['PLACED_ORDER_ID'];
-                                        $CUS_ID= $row['CUS_ID'];
-                                        $PLACED_ORDER_DATE = $row['PLACED_ORDER_DATE'];
-                                        $PLACED_ORDER_TOTAL = $row['PLACED_ORDER_TOTAL'];
-                                        $DELIVERY_ADDRESS = $row['DELIVERY_ADDRESS'];
-                                        $DELIVERY_ADDRESS = $row['DELIVERY_DATE'];
-                                        $PLACED_ORDER_CONFIRMATION = $row['PLACED_ORDER_CONFIRMATION'];
-                                        $PAYMENT_METHOD = $row['PAYMENT_METHOD'];
-                                        $PLACED_ORDER_TOTAL = $row['PLACED_ORDER_TOTAL'];
-                                        ?>
-                                            <tr>
-                                                <td><?php echo $PLACED_ORDER_DATE; ?></td>
-                                                <td><?php echo $CUS_ID; ?></td>
-                                                <td><a href=""><?php echo $PLACED_ORDER_ID; ?></a></td>
-                                                <td><?php echo $PLACED_ORDER_TOTAL; ?></td>
-                                                <td><?php echo $PLACED_ORDER_TOTAL; ?></td>
-                                                <td>
-                                                </td>
-                                            </tr>
-                                        <?php
-                                    }
-
-                                }
-                                else
-                                {
-                                    echo "<div class='error'>No Order Found</div>";  
-                                }
-                            ?> -->
-                          </table> 
-                    </section>
-                    <section class="side-menu">
-                        <div class="group inventory">
-                            <h3>Wholesale Users</h3>
-                            <div class="position-notif">
-                                <a href="" class="view">Accepted</a>
-                               <p class="notif">+99</p>
-                            </div>
-                            <div class="position-notif">
-                                <a href="" class="view">New</a>
-                                <p class="notif">+99</p>
-                            </div>
+                    </table>
+                </section>
+                <section class="side-menu">
+                    <div class="group inventory">
+                        <h3>Wholesale Users</h3>
+                        <div class="position-notif">
+                            <a href="" class="view">Accepted</a>
+                            <p class="notif">+99</p>
                         </div>
-                    </section>
+                        <div class="position-notif">
+                            <a href="" class="view">New</a>
+                            <p class="notif">+99</p>
+                        </div>
+                    </div>
                 </section>
             </section>
-        </main>
-        <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-    </body>
+        </section>
+    </main>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+</body>
+
 </html>
+<?php
+
+if (isset($_POST['accept'])) {
+    $sql = "UPDATE wholesaler SET WHL_STATUS = 'Accepted' WHERE WHL_ID = $WHL_ID";
+
+    $res2 = mysqli_query($conn, $sql);
+    header('location:admin-accepted-wholesale-users.php');
+}
+
+if (isset($_POST['reject'])) {
+    $sql = "UPDATE wholesaler SET WHL_STATUS = 'Rejected' WHERE WHL_ID = $WHL_ID";
+
+    $res2 = mysqli_query($conn, $sql);
+    header('location:admin-accepted-wholesale-users.php');
+} 
+?>
--- Website/admin-application-details.php
+++ Website/admin-application-details.php
@@ -2,99 +2,123 @@
 
 @include 'constants.php';
 
-$PRSN_ID = $_SESSION['prsn_id'];
+$WHL_ID = $_GET['WHL_ID'];
+$PRSN_NAME = $_GET['PRSN_NAME'];
+$PRSN_EMAIL = $_GET['PRSN_EMAIL'];
+$WHL_IMAGE = $_GET['WHL_IMAGE'];
+
+if (isset($_POST['accept'])) {
+    $sql = "UPDATE wholesaler SET WHL_STATUS = 'Accepted' WHERE WHL_ID = $WHL_ID";
+
+
+    $res2 = mysqli_query($conn, $sql);
+    header('location:admin-accepted-wholesale-users.php');
+}
+
+if (isset($_POST['reject'])) {
+    $sql = "UPDATE wholesaler SET WHL_STATUS = 'Rejected' WHERE WHL_ID = $WHL_ID";
+
+    $res2 = mysqli_query($conn, $sql);
+    header('location:admin-accepted-wholesale-users.php');
+}
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!--change title-->
-        <title>XXXX Application Details | Admin</title>
-        <link rel="stylesheet" href="header-styles.css">
-        <link rel="stylesheet" href="admin-styles.css"><!--change css file-->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet"> 
-        <script src="app.js" defer></script>
-    </head>
-    <body>
-        <header class="backend">
-            <div class="header-container">
-                <div class="website-title">
-                    <img id="logo" src="images/client-logo.jpg">
-                    <div class="text">
-                        <h1>Fat Rap's Barbeque's Online Store</h1>
-                        <p>ADMIN</p>
-                    </div>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--change title-->
+    <title>XXXX Application Details | Admin</title>
+    <link rel="stylesheet" href="header-styles.css">
+    <link rel="stylesheet" href="admin-styles.css"><!--change css file-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <script src="app.js" defer></script>
+</head>
+
+<body>
+    <header class="backend">
+        <div class="header-container">
+            <div class="website-title">
+                <img id="logo" src="images/client-logo.jpg">
+                <div class="text">
+                    <h1>Fat Rap's Barbeque's Online Store</h1>
+                    <p>ADMIN</p>
                 </div>
-                <nav>
-                    <ul>
-                        <li><a href="<?php echo SITEURL ;?>admin-home.php">Home</a></li>
-                        <li><a href="<?php echo SITEURL ;?>admin-edit-menu.php">Menu</a></li>
-                        <li><a href="<?php echo SITEURL ;?>admin-new-orders.php">Orders</a></li>
-                        <?php
-                            if(isset($_SESSION['prsn_id'])){
-                        ?>  
-                            <li><a href="<?php echo SITEURL ;?>logout.php">Logout</a><li>
-                        <?php
-                            } 
-                            else 
-                            {
-                        ?>
-                            <li><a href="<?php echo SITEURL ;?>login-page.php">Login</a></li>
-                        <?php
-                            }
-                        ?>
-                    </ul>
-                </nav>
             </div>
-        </header>
-        <main>
-            <section class="section">
-                <div class="section-heading row">
-                    <h2>Application Details</h2>
-                    <p>#XXXX</p>
-                </div> 
-                <section class="section-body">
-                    <section class="main-section column">
-                        <section class="table-wrapper">
-                            <table class="app-det-tbl">
-                                <tr>
-                                    <td class="bold-font top">Name:</td>
-                                    <td class="top">Juan Dela Cruz</td>
-                                </tr>
-                                <tr>
-                                    <td class="bold-font bottom">Email:</td>
-                                    <td class="bottom">juandelacruz@email.com</td>
-                                </tr>
-                            </table> 
-                        </section>
-                        <section class="column valid-id">
-                            <p class="bold-font">Valid ID</p>
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Philippine_passport_%282016_edition%29_data_page.jpg/190px-Philippine_passport_%282016_edition%29_data_page.jpg">
-                            <a href="">Open in new tab</a>
-                        </section>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo SITEURL; ?>admin-home.php">Home</a></li>
+                    <li><a href="<?php echo SITEURL; ?>admin-edit-menu.php">Menu</a></li>
+                    <li><a href="<?php echo SITEURL; ?>admin-new-orders.php">Orders</a></li>
+                    <?php
+                    if (isset($_SESSION['prsn_id'])) {
+                    ?>
+                        <li><a href="<?php echo SITEURL; ?>logout.php">Logout</a>
+                        <li>
+                        <?php
+                    } else {
+                        ?>
+                        <li><a href="<?php echo SITEURL; ?>login-page.php">Login</a></li>
+                    <?php
+                    }
+                    ?>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section class="section">
+            <div class="section-heading row">
+                <h2>Application Details</h2>
+                <p>#<?php echo $WHL_ID ?></p>
+            </div>
+            <section class="section-body">
+                <section class="main-section column">
+                    <section class="table-wrapper">
+                        <table class="app-det-tbl">
+                            <tr>
+                                <td class="bold-font top">Name:</td>
+                                <td class="top"><?php echo $PRSN_NAME ?></td>
+                            </tr>
+                            <tr>
+                                <td class="bold-font bottom">Email:</td>
+                                <td class="bottom"><?php echo $PRSN_EMAIL ?></td>
+                            </tr>
+                        </table>
                     </section>
-                    <section class="side-menu">
-                        <div class="group">
-                            <h3>Wholesale Users</h3>
-                            <div class="position-notif">
-                                <a href="" class="view">Total Count</a>
-                               <p class="notif">+99</p>
-                            </div>
-                        </div>
-                        <div class="group">
-                            <h3>Actions</h3>
-                            <a href="" class="view accept">Accept</a>
-                            <a href="" class="view reject">Reject</a>
-                        </div>
+                    <section class="column valid-id">
+                        <p class="bold-font">Valid ID</p>
+                        <img src="<?php echo SITEURL; ?>images/<?php echo $WHL_IMAGE; ?>" alt="">
                     </section>
                 </section>
+                <section class="side-menu">
+                    <div class="group">
+                        <h3>Wholesale Users</h3>
+                        <div class="position-notif">
+                            <a href="" class="view">Total Count</a>
+                            <p class="notif">+99</p>
+                        </div>
+                    </div>
+                    <div class="group">
+                        <h3>Actions</h3>
+                        <!-- <a href="" class="view accept">Accept</a>
+                            <a href="" class="view reject">Reject</a> -->
+                        <form method="POST">
+                            <button name="accept" class="page-button">Accept</button>
+                            <button name="reject" class="page-button">Reject</button>
+                        </form>
+
+                        <button>
+                    </div>
+                </section>
             </section>
-        </main>
-    </body>
+        </section>
+    </main>
+</body>
+
 </html>
