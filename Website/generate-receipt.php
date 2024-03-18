00b94057a168e7281661f3d1a872fd56e9b5dbd9
Darrie Angelo <darriedemesa@gmail.com>
2024-03-18 16:02:09 +0800

    backend for generate receipt ready

--- Website/generate-receipt.php
+++ Website/generate-receipt.php
@@ -4,49 +4,67 @@
 
 require('fpdf185/fpdf.php');
 
-$pdf = new FPDF('P', 'mm', "A4");
+class PDF extends FPDF
+{
+    function Header()
+    {
+        $this->Image('images/client-logo.png', 10, 6, 30);
+        $this->SetFont('Arial', 'B', 15);
+        $this->Cell(40);
+        $this->Cell(30, 10, 'Fat Rap BBQ', 0, 1, 'C');
+
+        $this->SetFont('Arial', "I", 8);
+        $this->Cell(38);
+        $this->Cell(30, 5, '123 Magaling St.,', 0, 1);
+        $this->Cell(38);
+        $this->Cell(30, 5, 'Brgy. Something, Somewhere City', 0, 1);
+        $this->Cell(38);
+        $this->Cell(30, 5, 'Contact Number', 0, 0);
+        $this->Ln(20);
+
+        $width=$this->GetPageWidth();
+        $height=$this->GetPageHeight();
+        $this->Line(13,20*2,$width-13,20*2);
+    }
+
+    function Footer()
+    {
+        $this->SetY(-15);
+        $this->SetFont('Arial', 'B', 8);
+        $this->Cell(0, 10, 'Page' . $this->PageNo() . '/{nb}', 0, 0, 'C');
+        $this->Ln(20);
+    }
+}
+
+
+$pdf = new PDF('P', 'mm', "A4");
+$pdf->AliasNbPages();
 
 $pdf->AddPage();
 
-$pdf->SetFont('Arial', "B", 20);
-$pdf->Cell(71, 10, '', 0, 0);
-$pdf->Cell(59, 5, 'Receipt', 0, 0);
-$pdf->Cell(59, 10, '', 0, 1);
-
 $pdf->SetFont('Arial', "B", 15);
-$pdf->Cell(71, 10, 'FatRapBBQ', 0, 0);
-$pdf->Cell(59, 5, '', 0, 0);
-$pdf->Cell(59, 10, 'Details', 0, 1);
-
-$pdf->SetFont('Arial', "B", 10);
-$pdf->Cell(130, 5, '123 Magaling St.,', 0, 0);
-$pdf->Cell(25, 5, 'Customer ID:', 0, 0);
-$pdf->Cell(34, 5, '0012', 0, 1);
-
-$pdf->SetFont('Arial', "B", 10);
-$pdf->Cell(130, 5, 'Brgy. Something, Somewhere City', 0, 0);
-$pdf->Cell(25, 5, 'Invoice Date:', 0, 0);
-$pdf->Cell(34, 5, '12th Jan 2019', 0, 1);
-
+$pdf->Cell(2);
+$pdf->Cell(130, 5, 'Order Details', 0, 0);
+$pdf->Cell(59, 5, 'Ship To', 0, 1);
+$pdf->SetFont('Arial', "", 10);
+$pdf->Cell(2);
+$pdf->Cell(130, 5, 'Order #: Temp Order Number', 0, 0);
+$pdf->Cell(59, 5, 'Name:', 0, 1);
+$pdf->Cell(2);
+$pdf->Cell(130, 5, 'Date: Temp Date ', 0, 0);
+$pdf->Cell(59, 5, 'Contact Number:', 0, 1);
+$pdf->Cell(2);
+$pdf->Cell(130, 5, 'Time: Temp Time ', 0, 0);
+$pdf->Cell(59, 5, 'Email:', 0, 1);
+$pdf->Cell(2);
 $pdf->Cell(130, 5, '', 0, 0);
-$pdf->Cell(25, 5, 'Invoice No:', 0, 0);
-$pdf->Cell(34, 5, 'ORD001', 0, 1);
-
-$pdf->SetFont('Arial', "B", 15);
-$pdf->Cell(59, 5, 'Bill To', 0, 0);
-$pdf->Cell(59, 5, '', 0, 1);
+$pdf->Cell(59, 5, 'Delivery Date:', 0, 1);
+$pdf->Cell(2);
 
 $pdf->SetFont('Arial', "B", 10);
-$pdf->Cell(130, 5, 'Cus Name:', 0, 1);
-$pdf->Cell(25, 5, 'Delivery Address:', 0, 1);
-$pdf->Cell(34, 5, 'Delivery Date:', 0, 1);
+$pdf->Cell(189, 5, '', 0, 1);
 
-
-
-$pdf->SetFont('Arial', "B", 10);
-$pdf->Cell(189, 10, '', 0, 1);
-
-$pdf->Cell(50, 10, '', 0, 1);
+$pdf->Cell(50, 5, '', 0, 1);
 $pdf->SetFont('Arial', "B", 10);
 
 /*Heading of table*/
@@ -74,7 +92,7 @@ if ($count > 0) {
         $IN_ORDER_QUANTITY = $row['IN_ORDER_QUANTITY'];
         $IN_ORDER_TOTAL = $row['IN_ORDER_TOTAL'];
 
-        
+
         $pdf->Cell(80, 6, $FOOD_NAME, 1, 0);
         $pdf->Cell(23, 6, $IN_ORDER_QUANTITY, 1, 0, 'R');
         $pdf->Cell(40, 6, $FOOD_PRICE, 1, 0, 'R');
