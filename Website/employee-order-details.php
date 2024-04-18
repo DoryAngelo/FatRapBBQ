55c0ad0a6419d9de067f56913788ec1c57ef3a75
htrcrd <117492863+htrcrd@users.noreply.github.com>
2024-04-18 17:18:50 +0800

    made placeholder text lighter in checkout page's customer note. fixed employee order details

--- Website/employee-order-details.php
+++ Website/employee-order-details.php
@@ -64,134 +64,138 @@ $PLACED_ORDER_ID = $_GET['PLACED_ORDER_ID'];
         </div>
     </header>
     <main>
-        <section class="section">
-            <a href="#" onclick="window.history.back();" class="back">Back</a>
-            <section class="block order-details">
-                <div class="scroll">
-                    <h2>Order Details</h2>
-                    <?php
-                    $sql = "SELECT * FROM placed_order WHERE PLACED_ORDER_ID = '$PLACED_ORDER_ID'";
-                    $res = mysqli_query($conn, $sql);
-                    $count = mysqli_num_rows($res);
-                    if ($count > 0) {
-                        while ($row = mysqli_fetch_assoc($res)) {
-                            $PLACED_ORDER_ID = $row['PLACED_ORDER_ID'];
-                            $PRSN_ID = $row['PRSN_ID'];
-                            $CUS_NAME = $row['CUS_NAME'];
-                            $CUS_NUMBER = $row['CUS_NUMBER'];
-                            $CUS_EMAIL = $row['CUS_EMAIL'];
-                            $PLACED_ORDER_DATE = $row['PLACED_ORDER_DATE'];
-                            $PLACED_ORDER_TOTAL = $row['PLACED_ORDER_TOTAL'];
-                            $DELIVERY_ADDRESS = $row['DELIVERY_ADDRESS'];
-                            $DELIVERY_DATE = $row['DELIVERY_DATE'];
-                            $PLACED_ORDER_STATUS = $row['PLACED_ORDER_STATUS'];
-                            $REFERENCE_NUMBER = $row['REFERENCE_NUMBER'];
-                            $PLACED_ORDER_TRACKER = $row['PLACED_ORDER_TRACKER'];
-                    ?>
-                            <div>
-                                <table class="contact-info">
-                                    <tr>
-                                        <th>Order #:</th>
-                                        <td><?php echo $PLACED_ORDER_ID ?></td>
-                                    </tr>
-                                    <tr>
-                                        <th>Order Status:</th>
-                                        <td><?php echo $PLACED_ORDER_STATUS ?></td>
-                                    </tr>
-                                    <tr>
-                                        <th>Order Tracker:</th>
-                                        <td><?php echo $PLACED_ORDER_TRACKER ?></td>
-                                    </tr>
-                                    <tr>
-                                        <th>Date and time placed:</th>
-                                        <td><?php echo $PLACED_ORDER_DATE ?></td>
-                                    </tr>
-                                    <tr>
-                                        <th>Name:</th>
-                                        <td><?php echo $CUS_NAME ?></td>
-                                    </tr>
-                                    <tr>
-                                        <th>Contact number:</th>
-                                        <td><?php echo $CUS_NUMBER ?></td>
-                                    </tr>
-                                    <tr>
-                                        <th>Email:</th>
-                                        <td><?php echo $CUS_EMAIL ?></td>
-                                    </tr>
-                                </table>
-                            </div>
-                            <div class="delivery-grp">
-                                <div class="text-grp">
-                                    <h3>Delivery address:</h3>
-                                    <p><?php echo $DELIVERY_ADDRESS ?></p>
-                                </div>
-                                <div class="text-grp">
-                                    <h3>Delivery date:</h3>
-                                    <p><?php echo $DELIVERY_DATE ?></p>
-                                </div>
-                                <div class="text-grp">
-                                    <h3>Reference Number:</h3>
-                                    <p><?php echo $REFERENCE_NUMBER ?></p>
-                                </div>
-                            </div>
-                            <div>
-                                <table class="order">
-                                    <thead>
-                                        <tr>
-                                            <th class="item-col">Item</th>
-                                            <th>Quantity</th>
-                                            <th class="subtotal-col">Sub Total</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $sql = "SELECT IN_ORDER_ID, FOOD_NAME, FOOD_PRICE, PRSN_ID, IN_ORDER_QUANTITY, IN_ORDER_TOTAL 
-FROM food, in_order WHERE food.FOOD_ID = in_order.FOOD_ID AND IN_ORDER_STATUS != 'Delivered' AND PLACED_ORDER_ID = '$PLACED_ORDER_ID'";
+        <section class="section" id="order-details">
+            <div class="container">
+                <div class="section-wrapper column">
+                    <a href="#" onclick="window.history.back();" class="back">Back</a>
+                    <section class="block order-details">
+                        <div class="scroll">
+                            <h2>Order Details</h2>
+                            <?php
+                            $sql = "SELECT * FROM placed_order WHERE PLACED_ORDER_ID = '$PLACED_ORDER_ID'";
+                            $res = mysqli_query($conn, $sql);
+                            $count = mysqli_num_rows($res);
+                            if ($count > 0) {
+                                while ($row = mysqli_fetch_assoc($res)) {
+                                    $PLACED_ORDER_ID = $row['PLACED_ORDER_ID'];
+                                    $PRSN_ID = $row['PRSN_ID'];
+                                    $CUS_NAME = $row['CUS_NAME'];
+                                    $CUS_NUMBER = $row['CUS_NUMBER'];
+                                    $CUS_EMAIL = $row['CUS_EMAIL'];
+                                    $PLACED_ORDER_DATE = $row['PLACED_ORDER_DATE'];
+                                    $PLACED_ORDER_TOTAL = $row['PLACED_ORDER_TOTAL'];
+                                    $DELIVERY_ADDRESS = $row['DELIVERY_ADDRESS'];
+                                    $DELIVERY_DATE = $row['DELIVERY_DATE'];
+                                    $PLACED_ORDER_STATUS = $row['PLACED_ORDER_STATUS'];
+                                    $REFERENCE_NUMBER = $row['REFERENCE_NUMBER'];
+                                    $PLACED_ORDER_TRACKER = $row['PLACED_ORDER_TRACKER'];
+                            ?>
+                                    <div>
+                                        <table class="contact-info">
+                                            <tr>
+                                                <th>Order #:</th>
+                                                <td><?php echo $PLACED_ORDER_ID ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Order Status:</th>
+                                                <td><?php echo $PLACED_ORDER_STATUS ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Order Tracker:</th>
+                                                <td><?php echo $PLACED_ORDER_TRACKER ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Date and time placed:</th>
+                                                <td><?php echo $PLACED_ORDER_DATE ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Name:</th>
+                                                <td><?php echo $CUS_NAME ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Contact number:</th>
+                                                <td><?php echo $CUS_NUMBER ?></td>
+                                            </tr>
+                                            <tr>
+                                                <th>Email:</th>
+                                                <td><?php echo $CUS_EMAIL ?></td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <div class="delivery-grp">
+                                        <div class="text-grp">
+                                            <h3>Delivery address:</h3>
+                                            <p><?php echo $DELIVERY_ADDRESS ?></p>
+                                        </div>
+                                        <div class="text-grp">
+                                            <h3>Delivery date:</h3>
+                                            <p><?php echo $DELIVERY_DATE ?></p>
+                                        </div>
+                                        <div class="text-grp">
+                                            <h3>Reference Number:</h3>
+                                            <p><?php echo $REFERENCE_NUMBER ?></p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <table class="order">
+                                            <thead>
+                                                <tr>
+                                                    <th class="item-col">Item</th>
+                                                    <th>Quantity</th>
+                                                    <th class="subtotal-col">Sub Total</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                $sql = "SELECT IN_ORDER_ID, FOOD_NAME, FOOD_PRICE, PRSN_ID, IN_ORDER_QUANTITY, IN_ORDER_TOTAL 
+        FROM food, in_order WHERE food.FOOD_ID = in_order.FOOD_ID AND IN_ORDER_STATUS != 'Delivered' AND PLACED_ORDER_ID = '$PLACED_ORDER_ID'";
 
-                                        $res = mysqli_query($conn, $sql);
+                                                $res = mysqli_query($conn, $sql);
 
-                                        $count = mysqli_num_rows($res);
+                                                $count = mysqli_num_rows($res);
 
-                                        if ($count > 0) {
-                                            while ($row = mysqli_fetch_assoc($res)) {
-                                                if ($row['PRSN_ID'] == $PRSN_ID) {
-                                                    $IN_ORDER_ID = $row['IN_ORDER_ID'];
-                                                    $FOOD_NAME = $row['FOOD_NAME'];
-                                                    $FOOD_PRICE = $row['FOOD_PRICE'];
-                                                    $IN_ORDER_QUANTITY = $row['IN_ORDER_QUANTITY'];
-                                                    $IN_ORDER_TOTAL = $row['IN_ORDER_TOTAL'];
-                                        ?>
+                                                if ($count > 0) {
+                                                    while ($row = mysqli_fetch_assoc($res)) {
+                                                        if ($row['PRSN_ID'] == $PRSN_ID) {
+                                                            $IN_ORDER_ID = $row['IN_ORDER_ID'];
+                                                            $FOOD_NAME = $row['FOOD_NAME'];
+                                                            $FOOD_PRICE = $row['FOOD_PRICE'];
+                                                            $IN_ORDER_QUANTITY = $row['IN_ORDER_QUANTITY'];
+                                                            $IN_ORDER_TOTAL = $row['IN_ORDER_TOTAL'];
+                                                ?>
 
-                                                    <tr>
-                                                        <td class="item-col"><?php echo $FOOD_NAME ?></td>
-                                                        <td><?php echo $IN_ORDER_QUANTITY ?></td>
-                                                        <td class="subtotal-col">₱<?php echo $IN_ORDER_TOTAL ?></td>
-                                                    </tr>
-                                        <?php
+                                                            <tr>
+                                                                <td class="item-col"><?php echo $FOOD_NAME ?></td>
+                                                                <td><?php echo $IN_ORDER_QUANTITY ?></td>
+                                                                <td class="subtotal-col">₱<?php echo $IN_ORDER_TOTAL ?></td>
+                                                            </tr>
+                                                <?php
 
+                                                        }
+                                                    }
                                                 }
-                                            }
-                                        }
-                                        $sql2 = "SELECT SUM(IN_ORDER_TOTAL) AS Total FROM  IN_ORDER WHERE PLACED_ORDER_ID = $PLACED_ORDER_ID";
-                                        $res2 = mysqli_query($conn, $sql2);
-                                        $row2 = mysqli_fetch_assoc($res2);
-                                        $total = $row2['Total'];
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div class="payment">
-                                <h3>Total Payment:</h3>
-                                <h3>₱<?php echo $total ?></h3>
-                            </div>
+                                                $sql2 = "SELECT SUM(IN_ORDER_TOTAL) AS Total FROM  IN_ORDER WHERE PLACED_ORDER_ID = $PLACED_ORDER_ID";
+                                                $res2 = mysqli_query($conn, $sql2);
+                                                $row2 = mysqli_fetch_assoc($res2);
+                                                $total = $row2['Total'];
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="payment">
+                                        <h3>Total Payment:</h3>
+                                        <h3>₱<?php echo $total ?></h3>
+                                    </div>
+                        </div>
+                <?php
+                                }
+                            }
+                ?>
+
+
+                    </section>
                 </div>
-        <?php
-                        }
-                    }
-        ?>
-
-
-            </section>
+            </div>
         </section>
     </main>
 </body>
